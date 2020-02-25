e2ece119fb5003ab63d22f61b4fa238abff4bf59
Julien Rollet <julien.rollet@fabernovel.com>
2020-02-25 11:56:09 +0100

    Implement get parks method

--- Classes/ParksAndRecRepository.swift
+++ Classes/ParksAndRecRepository.swift
@@ -6,12 +6,27 @@
 //
 
 import Foundation
+import Alamofire
 
 class ParksAndRecRepository {
 
     var allParksUrl = "https://download.data.grandlyon.com/ws/grandlyon/lyon.parcjardin_latest/all.json?maxfeatures=100&start=1"
 
-    func getAllParks() {
-        // Todo
+    private let restParksMapper = RestParksMapper()
+
+    func getAllParks(completion: @escaping (Result<[Park]>) -> Void) {
+        Alamofire.request(allParksUrl).responseJSON { responseJson in
+            if let error = responseJson.error {
+                completion(.failure(error))
+                return
+            }
+            guard
+                let json = responseJson.value as? [String : Any],
+                let restParks = RestParks(with: json) else {
+                    return
+            }
+            let parks = self.restParksMapper.mapToEntity(restParks)
+            completion(.success(parks))
+        }
     }
 }
