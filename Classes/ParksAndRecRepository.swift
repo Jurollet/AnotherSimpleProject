4cc848fa47bedb6484e2ac5a179351f77226e54a
Julien Rollet <julien.rollet@fabernovel.com>
2020-02-25 10:55:24 +0100

    Create parks and rec repository

---
+++ Classes/ParksAndRecRepository.swift
@@ -0,0 +1,17 @@
+//
+//  ParksAndRecRepository.swift
+//  AnotherSimpleProject
+//
+//  Created by Julien Rollet on 25/02/2020.
+//
+
+import Foundation
+
+class ParksAndRecRepository {
+
+    var allParksUrl = "https://download.data.grandlyon.com/ws/grandlyon/lyon.parcjardin_latest/all.json?maxfeatures=100&start=1"
+
+    func getAllParks() {
+        // Todo
+    }
+}
